cbdebe2396a585d98f4cc38c810744fa1d25d7a6
reetuTheCoder <codewithreetu@gmail.com>
2025-09-17 14:15:40 +0530

    remove extra file and setup for new

--- study-app/src/App.js
+++ study-app/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import Main from './Components/Study/PropTypes/Main';
-// import MyTodo from './Components/TodoApp/MyTodo';
+
 
 function App() {
   return (
     <div className="App">
-     {/* <MyTodo/> */}
-
-     <Main/>
+    
     </div>
   );
 }
